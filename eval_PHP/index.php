485fb834db9c4f5d0588d30f9c8627808d468794
Morgan <151154716+kyuming2005@users.noreply.github.com>
2024-09-18 09:51:30 +0200

    Add files via upload

---
+++ eval_PHP/index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $nom = 'Denis';
+        $prenom = 'Morgan';
+        $symfonyVersion = \Symfony\Component\HttpKernel\Kernel::VERSION;
+
+        return $this->render('home/index.html.twig', [
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'version' => $symfonyVersion,
+        ]);
+    }
+}
